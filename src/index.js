60d7203a0d8ca4fd1281665347ac593b4e5dd549
Eliza Cohen <elizacohen5@gmail.com>
2024-04-25 16:27:03 -0400

    complete task list mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById("create-task-form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    const list = document.getElementById("tasks")
+    const li = document.createElement("li");
+    const button = document.createElement("button");
+    button.textContent = "X";
+    li.textContent = event.target["new-task-description"].value;
+    console.log(li)
+    list.append(li);
+    li.append(button);
+    document.getElementById("create-task-form").reset()
+  })
+  document.getElementById("tasks").addEventListener("click", (event) => {
+    document.querySelector("li").remove();
+  })
 });
+
+  // add event listener to input, event is "submit" the form 
+  // with each new task, create a li element and append it to the ul 
+  // list item textContent is the event.target[].value
